a751ed73ec7cb90f9dd2c3667e5c2744609820ff
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-09-18 16:31:30 -0500

    first commit

---
+++ vue/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<style>
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+</style>
---
+++ vue/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter as createRouter, createWebHistory } from 'vue-router'
+import { useStore } from 'vuex'
+
+
+// Import components
+import Profile from '../views/Profile.vue'
+
+/**
+ * The Vue Router is used to "direct" the browser to render a specific view component
+ * inside of App.vue depending on the URL.
+ */
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView,
+  // },
+  {
+    path: "/",
+    component: Profile,
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/profile" },
+];
+
+// Create the router
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes
+});
+
+router.beforeEach((to) => {
+
+  // Get the Vuex store
+  const store = useStore();
+});
+
+export default router;
