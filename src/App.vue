b54c2a3995b5e3d93aebb8249e66b88dcef3b5ab
TMNTony <121651052+TMNTony@users.noreply.github.com>
2023-09-26 13:44:40 -0500

    New Icon

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app" class="min-h-screen bg-gray-300">
-    <router-view class="h-screen" />
+  <div id="app" class="min-h-screen bg-gray-300 flex flex-col justify-between">
+    <router-view class="flex-1" />
   </div>
 </template>
 <style>
